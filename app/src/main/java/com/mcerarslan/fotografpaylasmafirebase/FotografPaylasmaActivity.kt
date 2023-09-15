1317f10252fc6c28767d805a995b3f79fa364ca3
Mehmetcan Erarslan <mcerarslan@outlook.com>
2023-09-15 14:56:39 +0300

    login and firebase connected

---
+++ app/src/main/java/com/mcerarslan/fotografpaylasmafirebase/FotografPaylasmaActivity.kt
@@ -0,0 +1,11 @@
+package com.mcerarslan.fotografpaylasmafirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FotografPaylasmaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fotograf_paylasma)
+    }
+}
---
+++ app/src/main/java/com/mcerarslan/fotografpaylasmafirebase/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.mcerarslan.fotografpaylasmafirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val guncelKullanici = auth.currentUser
+        if (guncelKullanici != null) {
+            val intent = Intent(this,HaberlerActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+    }
+    fun girisYap(view: View){
+
+        auth.signInWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString()).addOnCompleteListener {
+            if (it.isSuccessful){
+
+                val guncelKullanici = auth.currentUser?.email.toString()
+                Toast.makeText(this,"Hoşgeldin ${guncelKullanici}",Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this,HaberlerActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this,it.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+
+    }
+    fun kayitOl(view: View){
+
+        val email = emailText.text.toString()
+        val sifre = passwordText.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,sifre).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent = Intent(this,HaberlerActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(applicationContext, it.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+}
